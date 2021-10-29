5298dfa9a94d9ce80647cd43f3b41b15d23ca22f
VlaSko1 <vizion84@yandex.ru>
2021-10-29 21:33:39 +0300

    Completed first and second tasks

--- src/task1/index.js
+++ src/task1/index.js
@@ -17,4 +17,9 @@ console.log('Record 5');
 Promise.resolve().then(() => Promise.resolve().then(() => console.log('Record 6')));
 
 
+
 // Мой вариант: 1, 5, 6, 2, 3, 4
+
+setTimeout(() => {
+  console.log("Развернутый ответ к первой задаче в текстовом виде находится в файле: ./src/task1/explanation.txt")
+}, 200);
