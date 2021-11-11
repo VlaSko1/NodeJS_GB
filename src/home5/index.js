483cbcce03bb0d962b2d608eb0889b84ddc5b8c8
VlaSko1 <vizion84@yandex.ru>
2021-11-11 19:29:28 +0300

    Add express, ejs

--- src/home5/index.js
+++ src/home5/index.js
@@ -1,9 +1,25 @@
 #!/usr/bin/env node
-const http = require('http');
+/*const http = require('http');
 const fs = require('fs');
+const path = require('path');*/
+const {showFileContents, getPath, getFile, getPattern} = require('./utils');
+const express = require('express');
 
-console.log("Server start 3001 port!");
-const server = http.createServer((request, response) => {
+const CWD = getPath();
+
+const app = express();
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+  const date = new Date();
+  res.render('index', {nowYear: date.getFullYear()});
+})
+app.listen(3000, 'localhost', () => {
+  
+});
+console.log("Server start 3000 port!");
+
+
+/*const server = http.createServer((request, response) => {
 
   if (request.method === 'GET') {
     response.writeHead(200, {
@@ -13,4 +29,4 @@ const server = http.createServer((request, response) => {
     readStream.pipe(response);
   }
   
-}).listen(3001, 'localhost');
+}).listen(3001, 'localhost');*/
