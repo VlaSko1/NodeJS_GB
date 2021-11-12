3605df0d62f1a97f892074324b7f0247b159c1a9
VlaSko1 <vizion84@yandex.ru>
2021-11-12 20:33:49 +0300

    Fix third home_work, completed home work 5

--- src/home5/index.js
+++ src/home5/index.js
@@ -1,32 +1,68 @@
 #!/usr/bin/env node
-/*const http = require('http');
+/*const http = require('http');*/
 const fs = require('fs');
-const path = require('path');*/
-const {showFileContents, getPath, getFile, getPattern} = require('./utils');
+const path = require('path');
+const { getPath, getNameFolder, getFileAndFolderNamesInDirectory, isFile } = require('./utils');
 const express = require('express');
 
-const CWD = getPath();
+let CWD = getPath();
+let folder = getNameFolder(CWD);
+let arrDir = getFileAndFolderNamesInDirectory(CWD);
+
 
 const app = express();
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
+  let query = req.query;
   const date = new Date();
-  res.render('index', {nowYear: date.getFullYear()});
-})
+  if (query.hasOwnProperty('name')) {
+    next();
+  } else {
+    res.render('index', {
+      nowYear: date.getFullYear(),
+      pathDir: CWD,
+      folder,
+      arrDir,
+      textFile: null,
+    });
+    res.status(200).end();
+  }
+}, (req, res, next) => {
+  let name = req.query.name
+  CWD = path.join(CWD, name);
+  if (isFile(CWD)) {
+    next();
+  } else {
+    folder = getNameFolder(CWD);
+    arrDir = getFileAndFolderNamesInDirectory(CWD);
+    const date = new Date();
+    res.render('index', {
+      nowYear: date.getFullYear(),
+      pathDir: CWD,
+      folder,
+      arrDir,
+      textFile: null,
+    });
+    res.status(200).end();
+  }
+}, (req, res, next) => {
+  let pathFile = CWD;
+  CWD = path.join(CWD, '..');
+  folder = getNameFolder(CWD);
+  arrDir = getFileAndFolderNamesInDirectory(CWD);
+  const date = new Date();
+    res.render('index', {
+      nowYear: date.getFullYear(),
+      pathDir: pathFile,
+      folder,
+      arrDir,
+      textFile: fs.readFileSync(pathFile, 'utf8'),
+    });
+    res.status(200).end();
+});
+
 app.listen(3000, 'localhost', () => {
-  
+
 });
 console.log("Server start 3000 port!");
 
-
-/*const server = http.createServer((request, response) => {
-
-  if (request.method === 'GET') {
-    response.writeHead(200, {
-      "Content-Type": 'text/html'
-    });
-    const readStream = fs.createReadStream('./src/home5/index.html', 'utf-8');
-    readStream.pipe(response);
-  }
-  
-}).listen(3001, 'localhost');*/
--- src/home5/utils.js
+++ src/home5/utils.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const inquirer = require('inquirer');
 const { Transform } = require('stream');
 const { EOL } = require('os');
 
@@ -9,79 +8,15 @@ const isFile = (filepath) => {
     return fs.lstatSync(filepath).isFile();
 }
 
-const getFileNamesInDirectory = async (directory) => {
-
-    const itemsInDirectory = await new Promise((resolve) => {
-        fs.readdir(directory, (err, data) => {
-            resolve(data);
-        });
-    });
-
-    return itemsInDirectory;
-}
-
-const promptUser = async (choices) => {
-    const optionKey = 'optionKey';
-
-    const result = await inquirer.prompt([{
-        name: optionKey,
-        type: 'list',
-        message: 'Please choose a catalog or a file to read',
-        choices,
-    }]);
-
-    return result[optionKey];
-}
-
-const showFileContents = async (filepath, pattern) => {
-    if (!pattern) {
-        return new Promise((resolve) => {
-            const stream = fs.createReadStream(filepath, 'utf-8');
-            stream.on('end', resolve);
-            stream.pipe(process.stdout);
-        });
-    } else {
-        return new Promise((resolve) => {
-            const readStream = fs.createReadStream(filepath, 'utf-8');
-
-            const transformStream = new Transform({
-                transform(chunk, encoding, callback) {
-                    // Разбиваем чанк на массив
-                    let arrMatch = chunk.toString().match(/^(.*?)$/gm);
-
-                    // Ищем в полученном массиве совпадения и подставляем корректный перенос для текущей ОС.
-                    const rightArr = arrMatch.filter((el) => el.includes(pattern));
-                    let resultString = rightArr.join(`${EOL}`) + `${EOL}`;
-                    this.push(resultString);
-
-                    callback();
-                }
-            });
-            
-            readStream.pipe(transformStream).pipe(process.stdout);
-
-            readStream.on('error', (err) => console.log(err));
-
-            transformStream.on('error', (err) => console.log(err));
-
-            transformStream.on('end', resolve);
-        })
-    }
-}
-const getPattern = () => {
-    let pattern = null;
-    if (process.argv[2] === '--pattern' && process.argv[3] !== '') {
-        pattern = process.argv[3];
-    }
-    if (process.argv[3] === '--pattern' && process.argv[4] !== '') {
-        pattern = process.argv[4];
-    }
-    return pattern;
+const getFileAndFolderNamesInDirectory = (directory) => {
+    const arrDir = fs.readdirSync( directory);
+    arrDir.unshift('..');
+    return arrDir;
 }
 const getPath = () => {
     const CWD = process.cwd();
     let pathDir = process.argv[2];
-    if (pathDir === undefined || pathDir === '--pattern') {
+    if (pathDir === undefined) {
         return CWD;
     }
     try {
@@ -99,30 +34,25 @@ const getPath = () => {
         }
     }
     catch {
-        console.log(`super_program: нет доступа к ${pathDir}: Нет такой директории`);
+        console.log(`Web: нет доступа к ${pathDir}: Нет такой директории`);
         process.exit(0);
     }
 
 }
-const getFile = async (directory) => {
-
-    let fileNames = await getFileNamesInDirectory(directory);
-    let result = await promptUser(fileNames);
-    result = path.join(directory, result);
-    if (isFile(result)) {
-        return result;
+const getNameFolder = (path) => {
+    if (path.includes('/')) {
+        return path.slice(path.lastIndexOf('/') + 1);
     }
-
-    return getFile(result);
-}
+    if (path.includes('\\')) {
+        return path.slice(path.lastIndexOf('\\') + 1);
+    }
+};
 
 
 
 module.exports = {
-    getFileNamesInDirectory,
-    promptUser,
-    showFileContents,
+    getNameFolder,
     getPath,
-    getFile,
-    getPattern
+    getFileAndFolderNamesInDirectory, 
+    isFile
 }
